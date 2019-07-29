7bc6aea8d3039450d5a13eafa5d2d16b021f44f9
Alexander <nemechekalexander@gmail.com>
2019-07-29 05:29:35 +0300

    set interval

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -22,7 +22,7 @@ render(){
             </div>
             <h1 className = 'h3'> Gãsiti în cel mai scurt timp medicamentul sau produsul farmaceutic care cãutati!</h1>
             <h1 className = 'h3'>Cum functioneazã ? </h1>
-            <h1 className = 'h3'> Completati formularul de pe pagina Cautã ca sa fiti contactat direct de farmaciile locale care ofera produsul.</h1>
+            <h1 className = 'h3'> Completati formularul de pe pagina Cautã ca sã fiti contactat direct de farmaciile locale care oferã produsul.</h1>
             <h1 className = 'h3'> Farmaciile din reteaua noastrã asteaptã mesajul dumneavoastrã!.</h1>
             <h1 className = 'h2'>Contact: <br/><br/>Cristian Lazar <br/>  Telefon: 0773853041</h1>
             
