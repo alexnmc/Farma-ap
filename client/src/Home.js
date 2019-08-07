c71f96bf9a41a81656a8cf8f380e05828f00ad24
Alexander <nemechekalexander@gmail.com>
2019-08-07 20:44:16 +0300

    loading animation adjusted

--- client/src/Home.js
+++ client/src/Home.js
@@ -62,7 +62,7 @@ class Home extends Component {
     onCameraError = (error) =>{
         alert('Vã rugã sã activați camera foto!')
         window.location.reload()
-      }
+    }
 
 
     render(){
