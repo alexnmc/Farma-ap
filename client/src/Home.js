4a02f33f86bac87992fccab21e5b094276262df6
Alexander <nemechekalexander@gmail.com>
2019-08-07 20:27:51 +0300

    loading animation adjusted

--- client/src/Home.js
+++ client/src/Home.js
@@ -59,11 +59,10 @@ class Home extends Component {
         })
     }
 
-    //onCameraError = (error) => {
-       // alert(error);
-     // }
-
-    
+    onCameraError = (error) =>{
+        alert('Vã rugã sã activați camera foto!')
+        location.reload()
+      }
 
 
     render(){
@@ -142,7 +141,7 @@ class Home extends Component {
                                     isImageMirror = {false}
                                     idealFacingMode = {FACING_MODES.ENVIRONMENT}
                                     onTakePhoto = { (dataUri) => { this.props.onTakePhoto(dataUri)} }
-                                  //  onCameraError = { (error) => { this.onCameraError(error); } }
+                                    onCameraError = { (error) => { this.onCameraError(error); } }
                                     isDisplayStartCameraError = {false}
                                 />
                                 <button className = 'cameraButton' onClick = {() => this.toggle()}>înapoi</button>
