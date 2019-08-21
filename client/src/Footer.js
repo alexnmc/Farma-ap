b7733891026557c25850d74d4bbae3cbf0b5c7d0
Alexander <nemechekalexander@gmail.com>
2019-08-21 20:13:54 +0300

    lookign good

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -4,7 +4,11 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer" style = {{width:'100vw' , display:'flex', alignItems: 'center', justifyContent: 'center' , marginTop:'-18pt', position: 'absolute'}}>
-            <p className = 'bottomP' style = {{margin:'auto', color: 'grey' , fontFamily: 'arial', fontSize: '10.5pt', fontWeight: '100'}}>© 2019 Farma-up. All Rights Reserved.</p>
+            {document.documentElement.clientWidth > 1000 ? 
+             <p className = 'bottomP' style = {{margin:'auto', color: 'grey' , fontFamily: 'arial', fontSize: '10.5pt', fontWeight: '100'}}>© 2019 Farma-up. All Rights Reserved.</p>
+             :
+             <p className = 'bottomP' style = {{margin:'auto', color: 'grey' , fontFamily: 'arial', fontSize: '8pt', fontWeight: '100'}}>© 2019 Farma-up. All Rights Reserved.</p>
+            }
         </div>
     )
 }
