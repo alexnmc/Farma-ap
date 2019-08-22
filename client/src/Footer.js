eeee42091b02b4f25a15f8387e96087266e9cac5
Alexander <nemechekalexander@gmail.com>
2019-08-22 12:32:41 +0300

    update

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="footer" style = {{width:'100vw' , display:'flex', alignItems: 'center', justifyContent: 'center' , marginTop:'-18pt', position: 'absolute'}}>
+        <div className ="footer">
             {document.documentElement.clientWidth > 1000 ? 
              <p className = 'bottomP' style = {{margin:'auto', color: 'grey' , fontFamily: 'arial', fontSize: '10.5pt', fontWeight: '100'}}>© 2019 Farma-up. All Rights Reserved.</p>
              :
