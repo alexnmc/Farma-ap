985ac2ed88ce8a33de03cdb9273f22e353c4a666
Alexander <nemechekalexander@gmail.com>
2019-08-25 02:02:10 +0300

    update

--- client/src/ResetPassword.js
+++ client/src/ResetPassword.js
@@ -25,7 +25,7 @@ class Activation extends Component{
                 
                 <div className = "contact">
                     <div className = "loginForm2">
-                        <h2 className = "activH1">Mulțumin! Parola a fost schimbatã.</h2>
+                        <h2 className = "activH1">Mulțumim! Parola a fost schimbatã.</h2>
                         <Link style = {{fontSize: '15pt', fontWeight: '900'}} to = "/pharma">Login</Link>
                     </div>
                 </div>
