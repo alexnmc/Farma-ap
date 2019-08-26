317658c9be8994b1418643689a290561372e10e3
Alexander <nemechekalexander@gmail.com>
2019-08-26 15:24:30 +0300

    update for PC

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className ="footer">
             {document.documentElement.clientWidth > 1000 ? 
-             <p className = 'bottomP' style = {{margin:'auto', color: 'grey' , fontFamily: 'arial', fontSize: '10.5pt', fontWeight: '100'}}>© 2019 Farma-up. All Rights Reserved.</p>
+             <p className = 'bottomP' style = {{margin:'auto', color: 'grey' , fontFamily: 'arial', fontSize: '12px', fontWeight: '100'}}>© 2019 Farma-up. All Rights Reserved.</p>
              :
              <p className = 'bottomP' style = {{margin:'auto', color: 'grey' , fontFamily: 'arial', fontSize: '8pt', fontWeight: '100'}}>© 2019 Farma-up. All Rights Reserved.</p>
             }
