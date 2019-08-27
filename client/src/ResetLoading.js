a47447aa498e3605c59d2569e8e557f7158d4fbc
Alexander <nemechekalexander@gmail.com>
2019-08-27 18:35:30 +0300

    send link on failed login

--- client/src/ResetLoading.js
+++ client/src/ResetLoading.js
@@ -5,7 +5,7 @@ import ReactLoading from "react-loading"
 const ResetLoading = ({type, color}) => {
     
   return(
-    <div style = {{marginBottom:"-8.5pt"}}>
+    <div >
       <ReactLoading  type={'spin'} color={' rgb(0, 97, 207)'} height={'20pt'} width={'20pt'}/>
     </div>
   )
