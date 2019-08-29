f61718b1e7653fade55c4c72766c373780c4c202
Alexander <nemechekalexander@gmail.com>
2019-08-29 16:33:41 +0300

    update

--- client/src/About.js
+++ client/src/About.js
@@ -19,7 +19,7 @@ render(){
         <div className = "contact">
             <div className = "imgWrap2">
                 
-                <h1 className = 'farmaup'>Farma - up</h1>
+                <h1 className = 'farmaup'>Farma-app</h1>
             </div>
             <h1 className = 'h6'>Gãsiți în cel mai scurt timp medicamentul sau produsul farmaceutic care îl cãutați!</h1>
             <h1 className = 'h6'>Cum funcționeazã ? </h1>
