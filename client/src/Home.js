f3aea9224616f65e75870bc0dcab232eed68341e
Alexander <nemechekalexander@gmail.com>
2019-08-29 06:01:38 +0300

    update

--- client/src/Home.js
+++ client/src/Home.js
@@ -132,8 +132,7 @@ class Home extends Component {
                                     { !this.state.enlargeToggle && this.props.img.length && <h2 className = "savedImg">Pozã salvatã:</h2>}
                                         <div style = {{display: "block", margin:"auto"}}>
                                         <div style = {{display: 'flex', alignItems: "center" , justifyContent: 'space-between'}}>
-                                        {this.state.enlargeToggle && <p style = {{ cursor: "pointer" , fontFamily: "arial", color:"white", fontSize: "9pt" , fontWeight: '200'}} onClick = {() => this.enlargePicture()}>înapoi</p>
-                                        }
+                                        {this.state.enlargeToggle && <p style = {{ cursor: "pointer" , fontFamily: "arial", color:"white", fontSize: "9pt" , fontWeight: '200'}} onClick = {() => this.enlargePicture()}>înapoi</p>}
                                         {this.state.enlargeToggle && <button onClick = {()=> this.deleteImg()}className = 'deletePic'>șterge</button>}
                                         </div>
                                         <img className = 'savedPicture' alt = '' src = {this.props.img}  style = {this.state.enlargeToggle ? document.documentElement.clientWidth < 900 ? {width: '85vw' , height: '60vh'} : {width: '300pt' , height: '300pt'} : null} onClick = {() => this.enlargePicture()}/> 
