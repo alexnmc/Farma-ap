6fdb575d60e2e1426403097902ef8347ec218157
Alexander <nemechekalexander@gmail.com>
2019-08-29 16:40:08 +0300

    update

--- routes/mail.js
+++ routes/mail.js
@@ -77,7 +77,7 @@ mailRouter.post('/reset', (req, res) => {
 mailRouter.post('/activate', (req, res) => {
     
   const output = `
-          <h3>Bine ați venit in rețeaua farma-up!</h3>
+          <h3>Bine ați venit in rețeaua Farmaapp!</h3>
           <h4>Vã rugãm sã activați contul: <a href="https://farmacie-app.herokuapp.com/activation/${req.body.id}">click aici</a></h4>
         
     `
