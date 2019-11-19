d548dbedff49d8d7bfbaf88fd0c3152d5ee80fd1
alex <nemechekalexander@gmail.com>
2019-11-18 21:40:47 -0600

    update

--- client/src/PharmaPortal.js
+++ client/src/PharmaPortal.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {withPharma} from './PharmaProvider'
 import moment from 'moment'
-import { FaUser } from 'react-icons/fa'
+import { FaUser, FaPhone, FaEnvelope} from 'react-icons/fa'
 import ReactLoading from "react-loading"
 
 
@@ -43,9 +43,9 @@ class PharmaPortal extends Component {
                 item.toggle ?
                 
                 <div className = "messageContainer" key = {item._id} style = {{backgroundColor: item.rezolvat ? "rgb(38, 55, 82)" : null}}>
-                    <p className = "p1">Email: <a href = {`mailto:${item.email}`} style = {!item.rezolvat? {color: "blue"} : {color:'black'}}>{item.email}</a></p>
-                    <p className = "p5">Tel: <a href = {`tel: ${item.phone}`} style = {!item.rezolvat? {color: "blue"} : {color:"black"}}>{`0${item.phone}`}</a></p> 
-                    {item.img ? <img  onClick = {!item.rezolvat ? () => this.props.enlarge(item._id) : null} className = 'myImg' alt = '' src = {item.img} style = {item.rezolvat ? {cursor:'none'}: null}/> : <div style = {{width:'30pt', height: '20pt'}}></div>}
+                    <p className = "p1"><FaEnvelope/><a href = {`mailto:${item.email}`} style = {!item.rezolvat ? {color: "blue", marginLeft: "5pt"} : {color:'black', marginLeft: "5pt"}}>{item.email}</a></p>
+                    <p className = "p5"><FaPhone/><a href = {`tel: ${item.phone}`} style = {!item.rezolvat ? {color: "blue", marginLeft: "5pt"} : {color:"black", marginLeft: "5pt"}}>{`0${item.phone}`}</a></p> 
+                    {item.img ? <img onClick = {!item.rezolvat ? () => this.props.enlarge(item._id) : null} className = 'myImg' alt = '' src = {item.img} style = {item.rezolvat ? {cursor:'none'}: null}/> : <div style = {{width:'30pt', height: '20pt'}}></div>}
                     <p className = "p3" style = {item.rezolvat ?  {color: "black"} : null}><span>Cautã: </span>{item.medication}</p>
                     <p className = "p4"> {moment(item.date).format('MMMM Do YYYY, HH:mm:ss ')}</p>
                     <p className = 'rezolvat' onClick = {!item.rezolvat ? () => this.props.rezolvat(item._id, item.email) : null} style = {item.rezolvat ? {cursor:'none', color: "white"} : null}>{!item.rezolvat ? "închide" : "rezolvat"}</p>
@@ -54,8 +54,8 @@ class PharmaPortal extends Component {
                 :
                  
                 <div className = "messageContainer" key = {item._id} style = {{position: 'relative' , backgroundColor : 'lightgrey'}}>
-                    <p className = "p1">Email: <a  href = {`mailto:${item.email}`} style = {{color: "blue"}}>{item.email}</a></p>
-                    <p className = "p5">Tel: <a  href = {`tel: ${item.phone}`} style = {{color: "blue"}}>{`0${item.phone}`}</a></p> 
+                    <p className = "p1"><FaEnvelope/><a href = {`mailto:${item.email}`} style = {{color: "blue", marginLeft: "5pt"}}>{item.email}</a></p>
+                    <p className = "p5"><FaPhone/><a href = {`tel: ${item.phone}`} style = {{color: "blue", marginLeft: "5pt"}}>{`0${item.phone}`}</a></p> 
                     <img    style = {document.documentElement.clientWidth < 1100 ?  
                                         {width: '100%' , height: '60vh', marginTop: '1%'} 
                                         : 
