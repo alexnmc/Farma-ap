d1fb64e83ab741250e3920d207293e206d7f992d
alex <nemechekalexander@gmail.com>
2019-11-19 22:51:06 -0600

    update

--- client/src/Home.js
+++ client/src/Home.js
@@ -10,8 +10,6 @@ import euflag from './Photos/euflag.png'
 import roflag from "./Photos/roflag.png"
 
 
-
-
 class Home extends Component {
     constructor(props){
         super(props)
@@ -20,6 +18,7 @@ class Home extends Component {
            enlargeToggle: false
         }
     }
+
 
     getFarmacies = (e) => {
         e.preventDefault()
