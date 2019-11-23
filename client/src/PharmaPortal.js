6ec9cff0dbdc7abc98f5fbd1cb00b997ead970b6
alex <nemechekalexander@gmail.com>
2019-11-22 20:52:47 -0600

    update

--- client/src/PharmaPortal.js
+++ client/src/PharmaPortal.js
@@ -76,45 +76,46 @@ class PharmaPortal extends Component {
         return(
             <div className = 'pharmaport'>
                 <div className = 'messageWrap'>
-                <div style = {{margin: "auto" , width: "97%" , display:'flex', alignItems: 'center', justifyContent: 'space-evenly', marginBottom: '5pt'}}>
-                    <p className = 'h1A' style = {{cursor: 'pointer' , color:"red"}}  onClick = {() => this.helpToggle()}>{this.state.helpToggle ? "utilizare" : "înapoi"}</p>
-                    <div style = {{display: "flex"}}>
-                        <FaUser/>
-                        <p className = 'h1A' style = {{marginLeft:'3pt'}}>{this.props.user.username}</p>
-                    </div>
-                </div>
-                <div className = 'portalWrap'>
-                    <div className = 'h1' style = {this.props.currentCity ? {opacity: 1} : {opacity:0}}>{this.props.messages.length === 1 ? '1 mesaj' : `${this.props.messages.length + ' mesaje'}`}</div>
-                    <input type = "text" className = 'input2' list="mylist" placeholder = 'Alege orașul:' value = {this.props.currentCity} onChange={this.props.handleChange} required/>
-                    <datalist id="mylist" >
-                        {this.props.cities.map((city, index) => <option key={city} value={city} className = {index}>{city}</option>)}
-                    </datalist>
-                    <button className = "logout" onClick = {this.props.logout}>ieșire</button>
-                </div>
-                {this.state.helpToggle ? 
-                    <div>
-                        {this.props.messageLoading ? 
-                        <div style = {{height: "50vh", width: "100vw", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-                            <ReactLoading  type={'balls'} color={'rgb(14, 25, 47)'} height={'40pt'} width={'40pt'} margin = {"auto"}/>
+                    <div style = {{margin: "auto" , width: "97%" , display:'flex', alignItems: 'center', justifyContent: 'space-evenly', marginBottom: '5pt'}}>
+                        <p className = 'h1A' style = {{cursor: 'pointer' , color:"red"}}  onClick = {() => this.helpToggle()}>{this.state.helpToggle ? "utilizare" : "înapoi"}</p>
+                        <div style = {{display: "flex"}}>
+                            <FaUser/>
+                            <p className = 'h1A' style = {{marginLeft:'3pt'}}>{this.props.user.username}</p>
                         </div>
-                        : 
-                        messages
-                        }
                     </div>
-
-                    :
-
-                    <div className = "pharmaport" style = {{marginTop: '15vh'}}>
-                        <ul>
-                            <li className = "help">Alegeți orașul în care doriți sa citiți mesaje</li>
-                            <li className = "help">După rezolvarea unui mesaj dați click pe <span style = {{color: "red"}}>închide</span></li>
-                            <li className = "help">Mesajele închise nu se pot re-deschide</li>
-                            <li className = "help">Pentru a mãri poza din mesaj dați click pe pozã</li>
-                        </ul>
+                    <div className = 'portalWrap'>
+                        <div className = 'h1' style = {this.props.currentCity ? {opacity: 1} : {opacity:0}}>{this.props.messages.length === 1 ? '1 mesaj' : `${this.props.messages.length + ' mesaje'}`}</div>
+                        <input type = "text" className = 'input2' list="mylist" placeholder = 'Alege orașul:' value = {this.props.currentCity} onChange={this.props.handleChange} required/>
+                        <datalist id="mylist" >
+                            {this.props.cities.map((city, index) => <option key={city} value={city} className = {index}>{city}</option>)}
+                        </datalist>
+                        <button className = "logout" onClick = {this.props.logout}>ieșire</button>
                     </div>
+                    {this.state.helpToggle ? 
+                        <div>
+                            {this.props.messageLoading ? 
+                            <div style = {{height: "50vh", width: "100vw", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+                                <ReactLoading  type={'balls'} color={'rgb(14, 25, 47)'} height={'40pt'} width={'40pt'} margin = {"auto"}/>
+                            </div>
+                            : 
+                            <div className = "scrollDiv">
+                            {messages}
+                          </div>
+                            }
+                        </div>
+
+                        :
+
+                        <div className = "pharmaport" style = {{marginTop: '15vh'}}>
+                            <ul>
+                                <li className = "help">Alegeți orașul în care doriți sa citiți mesaje</li>
+                                <li className = "help">După rezolvarea unui mesaj dați click pe <span style = {{color: "red"}}>închide</span></li>
+                                <li className = "help">Mesajele închise nu se pot re-deschide</li>
+                                <li className = "help">Pentru a mãri poza din mesaj dați click pe pozã</li>
+                            </ul>
+                        </div>
                     }
                 </div>
-               
             </div>
         )
     }
