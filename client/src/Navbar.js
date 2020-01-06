ae9ed42ff2eaa8e5dc0ed866d78e9ef8381f0177
alex <nemechekalexander@gmail.com>
2020-01-06 00:51:16 -0600

    update

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = (props) => {
             <Link onClick = {() => props.reloadRender2()} to="/">Cautã</Link>
             <Link to="/about">Despre</Link>
             <Link to="/contact">Contact</Link>
-            <Link className = 'b' onClick = {() => props.reloadRender3()} to = "/pharma">Cont</Link>
+            <Link className = 'b' onClick = {() => props.reloadRender3()} to = "/pharma">Login</Link>
         </div>
     )
 }
